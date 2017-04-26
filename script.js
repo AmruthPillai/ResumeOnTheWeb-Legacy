0cb07d2b4e27f3aec1db58dbcf795438b97374a4
Amruth Pillai <im.amruth@gmail.com>
2017-04-26 20:11:03 +0530

    finished language proficiency

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 $(function() {
   $('#lang-circle-eng').percircle({
+    progressBarColor: '#207bc2',
     text: 'ENGLISH'
+  });
+
+  $('#lang-circle-tam').percircle({
+    progressBarColor: '#e83d2c',
+    text: 'TAMIL'
+  });
+
+  $('#lang-circle-kan').percircle({
+    progressBarColor: '#e8a32c',
+    text: 'KANNADA'
   });
 });
